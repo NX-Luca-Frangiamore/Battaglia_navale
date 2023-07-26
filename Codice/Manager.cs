0bd4ba87d3a132565c17f79914e5f3d697762337
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-07-26 14:42:06 +0200

    interfaccie

---
+++ Codice/Manager.cs
@@ -0,0 +1,15 @@
+﻿namespace Battaglia_navale.Battaglia_navale
+{
+    interface IManager
+    {
+
+
+
+    }
+    interface Campo
+    {
+        public Nave get(int x, int y);//cerca una nave con posizione x,y
+        public void hit(int x, int y);//cerca ed in caso affonda nave
+
+    }
+}
---
+++ Codice/Nave.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Battaglia_navale
+{
+    interface INave
+    {
+        public bool shoot(int x,int y);
+        public bool move(int x, int y);
+        public bool checkParts(int x,int y);
+    }
+    record class Parts
+    {
+        public int x;
+        public int y;
+    }
+    class Nave : INave
+    {
+        public Dictionary<string, Parts> body;
+        public int x;
+        public int y;
+        private Manger manger;
+
+    }
+
+}
