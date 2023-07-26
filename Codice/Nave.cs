475c55baa64f4afe6015b9b539a567df6499247c
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-07-26 14:57:47 +0200

    metodi: move,getAssolutPOsition,checkHitted

--- Codice/Nave.cs
+++ Codice/Nave.cs
@@ -4,8 +4,11 @@ namespace Battaglia_navale
     interface INave
     {
         public bool shoot(int x,int y);
+        public bool hitted(int x, int y);
         public bool move(int x, int y);
-        public bool checkParts(int x,int y);
+        public (int, int) getAssolutPosition(int x,int y);//con x,y relativi
+        public (int, int) getAssolutPosition(Parts);
+        private bool checkParts(Parts);//controllo che le posizioni delle parti siano valide
     }
     record class Parts
     {
@@ -14,10 +17,30 @@ namespace Battaglia_navale
     }
     class Nave : INave
     {
-        public Dictionary<string, Parts> body;
+        public Dictionary<string, Parts> body;//permette di assemblare la nave, id="x,y"; x,y sono relativi
         public int x;
         public int y;
         private Manger manger;
+        public (int, int) getAssolutPosition(int x, int y) //con x,y relativi
+        {
+            return (this.x+x,this.y+y);
+        }
+        public bool hitted(int xr, int yr)
+        {
+            (int x, int y) = getAssolutPosition(xr, yr);
+            if (body.ContainsKey(x + "," + y)) return true;
+            return false;
+        }
+        public bool move(int x, int y)
+        {
+            foreach(var part in body)
+            {
+                if (checkPart(part) == false)
+                    return false;
+            }
+            return true;
+        }
+
 
     }
 
