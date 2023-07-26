92aa17bbe0c7e78ff9d058b37792e6a1444863e5
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-07-26 18:05:51 +0200

    fix shoot

--- Codice/Manager.cs
+++ Codice/Manager.cs
@@ -13,6 +13,7 @@ namespace Battaglia_navale
                 return i;
             }
             public int get() { return i; }
+            public int prevision() { return (i == 0) ? 1 : 0; }
         }
         public readonly int xMappa,yMappa;//dimensione mappa
         private Campo[] campi=new Campo[2];
@@ -20,22 +21,40 @@ namespace Battaglia_navale
         public Manager(int xMappa,int yMappa) {
             this.xMappa = xMappa;
             this.yMappa = yMappa;
+            campi[0] = new Campo();
+            campi[1] = new Campo(); 
+            t=new turno();  
         }
         public void loop()
         {
+            Console.WriteLine($"turno {t.get()}");
             campi[t.get()].loop();
             t.go();//alterno i due campi
         }
         public bool shoot(int x,int y)
         {
-            return campi[t.get()].hit(x,y);
+            return campi[t.prevision()].hit(x,y);
+        }
+        public void addNave(TypeBody t,int x,int y,int iC) { 
+            Nave n=new Nave(t,x,y, this);
+            campi[iC].Add(n);
+        }
+        public void addNave(int iC)
+        {
+            Nave n = new Nave(this);
+            campi[iC].Add(n);
+        
         }
 
     }
     class Campo
     {
         public List<Nave> flotta;
-       
+        public Campo()
+        {
+            flotta=new List<Nave> ();
+        }
+        public void Add(Nave n) => flotta.Add(n);
         public bool hit(int x, int y) {
             
             foreach(Nave n in flotta)
@@ -50,7 +69,7 @@ namespace Battaglia_navale
         {
             foreach (Nave n in flotta)
             {
-                n.shoot();
+                Console.WriteLine(n.shoot());
             }
         }
     }
---
+++ Codice/process.cs
@@ -0,0 +1,8 @@
+using Battaglia_navale;
+
+Manager m = new Manager(10,10);
+
+m.addNave(0);
+m.addNave(1);
+
+m.loop();
