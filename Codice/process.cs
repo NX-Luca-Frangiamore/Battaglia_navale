8e1e7b9ea4854ae30966bb86030951bfd67da52a
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-07-27 15:34:58 +0200

    classe game

--- Codice/process.cs
+++ Codice/process.cs
@@ -1,13 +1,32 @@
 using Battaglia_navale;
+using System.Security.Cryptography.X509Certificates;
 
-Manager m = new Manager(10,10);
+game g = new game();
+g.start(5);
 
 
 
-m.addNave(TypeBody.Lunga, 5, 0, 0);
-m.addNave(TypeBody.Larga, 0, 5, 1);
-m.printMappa();
-m.loop();
-m.addNave(TypeBody.Quadrata, 5, 3, 0);
-m.loop();
-m.printMappa();
+
+
+class game
+{
+
+    Manager m = new Manager(10, 10);
+    public void start(int nNavi)
+    {
+        start(nNavi,0);
+        start(nNavi, 1);
+    }
+    private void start(int nNavi, int campo) { 
+         for(int i = 0; i < nNavi; i++)
+         {
+            m.addNave(0);
+         }
+    }
+    public void loop()
+    {
+        m.loop();
+        m.printMappa();
+        this.loop();
+    }
+}
