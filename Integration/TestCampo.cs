ac968849432ef3065172ad563f20d2caf104359f
NX-Luca-Frangiamore <140600363+NX-Luca-Frangiamore@users.noreply.github.com>
2023-07-28 09:49:58 +0200

    Add files via upload

---
+++ Integration/TestCampo.cs
@@ -0,0 +1,55 @@
+﻿using Battaglia_navale;
+namespace Test.Integration
+{
+    public class TestCampo
+    {
+        [Fact]
+        public void aggiuntaNaveInCampo()
+        {
+            Manager.xMappa = 3;
+            Manager.yMappa = 3;
+            Campo campo= new Campo();
+            campo.Add(new Nave(TypeBody.Quadrata, 0, 0, null));
+            Assert.Equal(campo.printCaselle(), "False False False \nTrue True False \nTrue True False \n");
+           
+        }
+        [Fact]
+        public void affondareNave()
+        {
+            Manager.xMappa = 3;
+            Manager.yMappa = 3;
+            Campo campo = new Campo();
+            campo.Add(new Nave(TypeBody.Quadrata, 0, 0, null));
+
+            campo.hit(0, 0);
+            Assert.Equal(campo.printCaselle(), "False False False \nFalse False False \nFalse False False \n");
+
+        }
+
+
+        [Theory]
+        [InlineData(TypeBody.Quadrata, 3, 0)]
+        [InlineData(TypeBody.Lunga, 0, 2)]
+        public void fuoriuscitaMappa(TypeBody t, int x, int y)
+        {
+            Manager.xMappa = 4;
+            Manager.yMappa = 4;
+            Campo campo = new Campo();
+
+            Assert.False(campo.Add(new Nave(t, x, y, null)));
+        }
+
+        [Fact]
+        public void mancareNave()
+        {
+            Manager.xMappa = 3;
+            Manager.yMappa = 3;
+            Campo campo = new Campo();
+            campo.Add(new Nave(TypeBody.Quadrata, 0, 0, null));
+
+            campo.hit(2,2);
+            Assert.Equal(campo.printCaselle(), "False False False \nTrue True False \nTrue True False \n");
+
+        }
+    }
+}
---
+++ Unit/TestTypeNave.cs
@@ -0,0 +1,29 @@
+
+using Newtonsoft.Json;
+
+namespace Test.Unit
+{
+    public class TestTypeNave
+    {
+
+        [Fact]
+        public void ottenimentoBodyNave()
+        {
+            Dictionary<string, Parts> body = Type.getBody(TypeBody.Lunga);
+            Dictionary<string, Parts> bodyTest = new Dictionary<string, Parts>();
+            bodyTest["0,0"] = new Parts(0, 0);
+            bodyTest["0,1"] = new Parts(0, 1);
+            bodyTest["0,2"] = new Parts(0, 2);
+            Assert.Equal(JsonConvert.SerializeObject(body), JsonConvert.SerializeObject(bodyTest));
+
+
+            body = Type.getBody(TypeBody.Larga);
+            bodyTest = new Dictionary<string, Parts>();
+            bodyTest["0,0"] = new Parts(0, 0);
+            bodyTest["1,0"] = new Parts(1, 0);
+            bodyTest["2,0"] = new Parts(2, 0);
+            Assert.Equal(JsonConvert.SerializeObject(body), JsonConvert.SerializeObject(bodyTest));
+
+        }
+    }
+}
